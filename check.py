58f603d8aa9958941ed14bbfb22583d6bc2c1394
guillermo.bandres <guillermo.bandres@babel.es>
2019-05-19 20:39:31 +0200

    Cambios 16/05/19

--- check.py
+++ check.py
@@ -6,6 +6,8 @@ from glob import glob
 import cv2
 import numpy as np
 import os
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
 
 TRAIN_DIR = './data/asl_alphabet_train'
 TEST_DIR = './data/asl_alphabet_test'
@@ -14,9 +16,23 @@ classes_no = len(classes)
 
 prepare_data(train_dir=TRAIN_DIR, test_dir=TEST_DIR)
 print(classes)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True # dynamically grow the memory used on the GPU
+#config.log_device_placement = True # to log device placement (on which device the operation ran)
+# (nothing gets printed in Jupyter, only if you run it standalone)
+
+
+sess = tf.Session(config=config)
+#sess.run(tf.global_variables_initializer())
+
+set_session(sess) # set this TensorFlow session as the default session for Keras
+
 model = create_model(classes_no=classes_no)
 
-model.load_weights('hola.h5')
+model.summary()
+
+#model.load_weights('hola.h5')
 
 #img = cv2.imread('./data/asl_alphabet_test/N/N_test.jpg')
 #img = cv2.resize(img,(64,64))
@@ -25,7 +41,9 @@ model.load_weights('hola.h5')
 
 
 images = []
+names = []
 for img in os.listdir('./data/test/'):
+    names.append(img)
     #img = image.load_img(img, target_size=(64, 64))
     img = cv2.imread('./data/test/'+img)
     img = cv2.resize(img,(64,64))
@@ -37,7 +55,10 @@ for img in os.listdir('./data/test/'):
 images = np.vstack(images)
 
 
-res = model.predict_classes(images)
+#res = model.predict_classes(images)
 
 for i in range(len(res)):
-    print(classes[res[i]])
+    if names[i].split('_')[0] == classes[res[i]]:
+        print('OK: ' + names[i] + ' -> ' + classes[res[i]])
+    else:
+        print(names[i] + ' -> ' +  classes[res[i]])
